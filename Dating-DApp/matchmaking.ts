fa90fedc6f932c8cc9719165be4ea50c60c3a560
Noah Fuery <nfuery@chapman.edu>
2023-12-10 13:21:17 -0800

    aider: Added functions to add potential matches and accepted matches, and modified the load function to exclude already swiped profiles.
    # Aider chat conversation:
    
    ASSISTANT: To implement this, we need to modify the `Dating-DApp/matchmaking.ts` file and add the following functions:
    
    1. `addPotentialMatch`: This function will add a new user to the potentialMatches of all existing users if the hard constraints allow it.
    2. `addAcceptedMatch`: This function will add a user to the acceptedMatches of another user if both users like each other.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    Dating-DApp/matchmaking.ts
    ```typescript
    <<<<<<< SEARCH
    export const load = (async ({
        locals: {
            session: { user_id }
        }
    }) => {
        const feed = await prisma.profile.findMany({
            // add filters on city, interested in, etc.
            // exclude people already swiped on
            where: {
                NOT: {
                    swipedBy: {
                        some: { profileId: user_id }
                    }
                },
                id: {
                    not: user_id
                }
            }
        });
    
        return { feed };
    }) satisfies PageServerLoad;
    =======
    export const addPotentialMatch = async (newUser) => {
        const users = await prisma.profile.findMany();
        users.forEach(async (user) => {
            // Check hard constraints here
            // If constraints are met, add newUser to user's potentialMatches
            await prisma.profile.update({
                where: { id: user.id },
                data: { potentialMatches: { connect: { id: newUser.id } } },
            });
        });
    };
    
    export const addAcceptedMatch = async (user1, user2) => {
        // Check if both users like each other
        const user1LikesUser2 = user1.potentialMatches.includes(user2.id);
        const user2LikesUser1 = user2.potentialMatches.includes(user1.id);
    
        if (user1LikesUser2 && user2LikesUser1) {
            // If both users like each other, add them to each other's acceptedMatches
            await prisma.profile.update({
                where: { id: user1.id },
                data: { acceptedMatches: { connect: { id: user2.id } } },
            });
            await prisma.profile.update({
                where: { id: user2.id },
                data: { acceptedMatches: { connect: { id: user1.id } } },
            });
        }
    };
    
    export const load = (async ({
        locals: {
            session: { user_id }
        }
    }) => {
        const feed = await prisma.profile.findMany({
            // add filters on city, interested in, etc.
            // exclude people already swiped on
            where: {
                NOT: {
                    swipedBy: {
                        some: { profileId: user_id }
                    }
                },
                id: {
                    not: user_id
                }
            }
        });
    
        return { feed };
    }) satisfies PageServerLoad;
    >>>>>>> REPLACE
    ```
    
    Please note that this is a basic implementation and you might need to adjust it according to your specific needs and database schema. For example, you might need to add additional checks or modify the way how users are connected in the database.

--- Dating-DApp/matchmaking.ts
+++ Dating-DApp/matchmaking.ts
@@ -1,6 +1,36 @@
 import { prisma } from '$lib/server/prisma';
 import type { PageServerLoad } from './$types';
 
+export const addPotentialMatch = async (newUser) => {
+    const users = await prisma.profile.findMany();
+    users.forEach(async (user) => {
+        // Check hard constraints here
+        // If constraints are met, add newUser to user's potentialMatches
+        await prisma.profile.update({
+            where: { id: user.id },
+            data: { potentialMatches: { connect: { id: newUser.id } } },
+        });
+    });
+};
+
+export const addAcceptedMatch = async (user1, user2) => {
+    // Check if both users like each other
+    const user1LikesUser2 = user1.potentialMatches.includes(user2.id);
+    const user2LikesUser1 = user2.potentialMatches.includes(user1.id);
+
+    if (user1LikesUser2 && user2LikesUser1) {
+        // If both users like each other, add them to each other's acceptedMatches
+        await prisma.profile.update({
+            where: { id: user1.id },
+            data: { acceptedMatches: { connect: { id: user2.id } } },
+        });
+        await prisma.profile.update({
+            where: { id: user2.id },
+            data: { acceptedMatches: { connect: { id: user1.id } } },
+        });
+    }
+};
+
 export const load = (async ({
     locals: {
         session: { user_id }
